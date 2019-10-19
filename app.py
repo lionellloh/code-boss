6f7de3ddc40f66ad621b16c0bb8393744c2ebf7c
pengshanshan99 <pengshanshan99@DESKTOP-3K32GH2.localdomain>
2019-10-19 12:13:41 +0800

    front end first draft

--- app.py
+++ app.py
@@ -1,21 +1,5 @@
-from flask import Flask
-import googlemaps
-from datetime import datetime
-
-gmaps = googlemaps.Client(key='Add Your Key here')
-
-# Geocoding an address
-geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
-
-# Look up an address with reverse geocoding
-reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
-
-# Request directions via public transit
-now = datetime.now()
-directions_result = gmaps.directions("Sydney Town Hall",
-                                     "Parramatta, NSW",
-                                     mode="transit",
-                                     departure_time=now)
+from flask import render_template, url_for, redirect, Flask, render_template, request, session
+import json
 
 app = Flask(__name__)
 
@@ -24,11 +8,21 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
-
-@app.route('/places'):
-
-
-
+@app.route("/restaurant", methods=['POST','GET'])
+def book_list_page():
+    if request.method == 'POST':
+        if request.form['submit_button'] == 'Search':
+            latitude = request.form['field1']
+            longitude = request.form['field2']
+            radius = request.form['field3']
+            search_criteria = [latitude, longitude, radius]
+            # book_list = get_book([latitude, longitude, radius])
+            book_list = [{"address":"street1","name":"best rest"}]
+            print(search_criteria)
+            return render_template("restaurantList.html", results=book_list)
+    else:
+        book_list = [{"address":"street1","name":"best rest"}]
+        return render_template("restaurantList.html", results=book_list)
 
 if __name__ == '__main__':
     app.run()
