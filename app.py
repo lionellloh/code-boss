058bb45b3695c54111491a3584fff2ab01793ca4
Lionell Loh Jian An <lionell_loh@mymail.sutd.edu.sg>
2019-10-19 11:14:20 +0800

    txt files

--- app.py
+++ app.py
@@ -1,4 +1,21 @@
 from flask import Flask
+import googlemaps
+from datetime import datetime
+
+gmaps = googlemaps.Client(key='Add Your Key here')
+
+# Geocoding an address
+geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
+
+# Look up an address with reverse geocoding
+reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
+
+# Request directions via public transit
+now = datetime.now()
+directions_result = gmaps.directions("Sydney Town Hall",
+                                     "Parramatta, NSW",
+                                     mode="transit",
+                                     departure_time=now)
 
 app = Flask(__name__)
 
@@ -7,6 +24,8 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+
+@app.route('/places'):
 
 
 
